85f8c78d2197a86ca0f525ed50a4dad1f37d7d49
Michael Fuget <michaelfuget@gmail.com>
2020-12-02 09:34:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,13 +17,25 @@ class Song
   end
   def genres
     out = []
-        @@genres.each do |genre|
-      if(out.has_key?(genre))
-        out[genre] += 1 
-      else 
-        out[genre] = 1 
+    @@genres.each do |genre|
+      if(!out.include?(genre))
+        out.push(genre) 
       end 
+    end
+    return out 
+  end
+  
+  def artists
+    out = []
+    @@artists.each do |artist|
+      if(!out.include?(artist))
+        out.push(artist) 
+      end 
+    end
+    return out 
+  end
 
+  
   def genre_count
     out = {}
       @@genres.each do |genre|
